e39f48880cf4bef0a1e573865e4b6d8138335600
Kai Yao <ohkaiby@gmail.com>
2013-09-06 01:59:42 -0500

    fixed a syntax bug, updated some html

--- app/classes/helpers/email.php
+++ app/classes/helpers/email.php
@@ -14,9 +14,9 @@ class Email {
 
 		$html = 'Dear '. $fields[ 'first_name' ] .',';
 		$html .= '<p>This email is to confirm your registration for Grace Conference 2013. The total amount paid is $'. $fields[ 'amount_paid' ] .'.</p>';
-		$html .= '<p>Note that this does not cover your stay at the Pheasant Run Resort. You’ll need to register with PRR or a nearby hotel separately. Hotel registration discounts can be found at <a href="https://reservations.ihotelier.com/crs/g_reservation.cfm?groupID=794195&hotelID=2932">Pheasant Run at iHotelier</a>, <a href="http://www.ihg.com/holidayinnexpress/hotels/us/en/st.-charles/chisc/hoteldetail?groupCode=CCL">Holiday Inn Express</a>, and the <a href="http://cwp.marriott.com/chisc/gospelforchina/">Courtyard Mariott</a>.</p>';
+		$html .= '<p>Note that this does not cover your stay at the Pheasant Run Resort. You&apos;ll need to register with PRR or a nearby hotel separately. Hotel registration discounts can be found at <a href="https://reservations.ihotelier.com/crs/g_reservation.cfm?groupID=794195&hotelID=2932">Pheasant Run at iHotelier</a>, <a href="http://www.ihg.com/holidayinnexpress/hotels/us/en/st.-charles/chisc/hoteldetail?groupCode=CCL">Holiday Inn Express</a>, and the <a href="http://cwp.marriott.com/chisc/gospelforchina/">Courtyard Mariott</a>.</p>';
 		$html .= '<p>We look forward to seeing you!</p>';
-		$html .= '<br><br>Grace Conference 2013';
+		$html .= '<br>Grace Conference 2013';
 
 		$this->send( $fields[ 'email' ], 'Grace 2013 Registration Confirmation', $html );
 	}
@@ -27,7 +27,7 @@ class Email {
 		$config = $f3->get( 'config' );
 		require_once $config[ 'base_path' ] .'/app/libraries/PHPMailer/class.phpmailer.php';
 
-		$mail = new PHPMailer;
+		$mail = new \PHPMailer;
 
 		$mail->IsSMTP();
 		$mail->Host = 'smtp.mandrillapp.com';                 // Specify main and backup server
